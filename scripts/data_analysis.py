b0be0295b5e3b63718a9134e52e9021e886edf4b
Rubenjme <rubjaicab@gmail.com>
2025-03-13 20:35:38 +0100

    Se añade data_analysis.py que calcula KPIs del archivo generado por data_generation.py y guarda esos KPIs en supply_chain_kpis.csv

---
+++ scripts/data_analysis.py
@@ -0,0 +1,42 @@
+import os
+import pandas as pd
+
+script_dir = os.path.dirname(__file__)                     # Carpeta base del script
+data_dir = os.path.join(script_dir, '..', 'data')          # Ruta a la carpeta "data" (subiendo un nivel y luego /data)
+csv_path = os.path.join(data_dir, 'supply_chain_data.csv') # Obtengo la ruta del archivo CSV generado por data_generation.py
+
+df = pd.read_csv(csv_path) # Leo el CSV
+
+# Cálculo de KPIs
+total_orders = len(df)
+on_time_orders = len(df[df["OnTimeDelivery"] == "Sí"])
+on_time_percentage = (on_time_orders / total_orders) * 100
+avg_lead_time = df["LeadTime"].mean()
+avg_quality = df["QualityScore"].mean()
+avg_cost = df["Cost"].mean()
+
+
+kpi_data = { # Diccionario con los datos de los KPIs
+    "KPI": [
+        "Total Orders",
+        "On Time Delivery (%)",
+        "Average Lead Time (days)",
+        "Average Quality Score",
+        "Average Cost"
+    ],
+    "Value": [
+        total_orders,
+        round(on_time_percentage, 2),
+        round(avg_lead_time, 2),
+        round(avg_quality, 2),
+        round(avg_cost, 2)
+    ]
+}
+
+kpi_df = pd.DataFrame(kpi_data) # Creo un DataFrame con los KPIs
+
+
+kpi_csv_path = os.path.join(data_dir, 'supply_chain_kpis.csv') # Ruta al archivo CSV de KPIs (data/supply_chain_kpis.csv)
+kpi_df.to_csv(kpi_csv_path, index=False)                       # Guardo el DataFrame en un archivo CSV
+print("Análisis completado. KPIs guardados en:", kpi_csv_path) # Imprimo la ruta del archivo generado como referencia
+
