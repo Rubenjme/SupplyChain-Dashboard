e9fc65017622c265cc9b0e6f8f81569aaf3829c0
Rubenjme <rubjaicab@gmail.com>
2025-03-15 13:07:35 +0100

    Se añaden cambios en los .py de forma que puedan ser reconocidos adecuadamente al importarlos al archivo Excel. Se cambió la forma en que se generan los datos para que sean algo más realistas.

--- scripts/data_analysis.py
+++ scripts/data_analysis.py
@@ -5,7 +5,7 @@ script_dir = os.path.dirname(__file__)                     # Carpeta base del sc
 data_dir = os.path.join(script_dir, '..', 'data')          # Ruta a la carpeta "data" (subiendo un nivel y luego /data)
 csv_path = os.path.join(data_dir, 'supply_chain_data.csv') # Obtengo la ruta del archivo CSV generado por data_generation.py
 
-df = pd.read_csv(csv_path) # Leo el CSV
+df = pd.read_csv(csv_path, sep=';', decimal=',', encoding='utf-8') # Leo el CSV
 
 # Cálculo de KPIs
 total_orders = len(df)
@@ -16,27 +16,18 @@ avg_quality = df["QualityScore"].mean()
 avg_cost = df["Cost"].mean()
 
 
-kpi_data = { # Diccionario con los datos de los KPIs
-    "KPI": [
-        "Total Orders",
-        "On Time Delivery (%)",
-        "Average Lead Time (days)",
-        "Average Quality Score",
-        "Average Cost"
-    ],
-    "Value": [
-        total_orders,
-        round(on_time_percentage, 2),
-        round(avg_lead_time, 2),
-        round(avg_quality, 2),
-        round(avg_cost, 2)
-    ]
+kpis = { # Diccionario con los KPIs
+    "Total Orders": round(total_orders, 2),
+    "On Time Delivery (%)": round(on_time_percentage, 2),
+    "Average Lead Time (days)": round(avg_lead_time, 2),
+    "Average Quality Score": round(avg_quality, 2),
+    "Average Cost": round(avg_cost, 2)
 }
 
-kpi_df = pd.DataFrame(kpi_data) # Creo un DataFrame con los KPIs
+kpi_df = pd.DataFrame(list(kpis.items()), columns=["KPI", "Value"]) # Creo un DataFrame con los KPIs
 
 
 kpi_csv_path = os.path.join(data_dir, 'supply_chain_kpis.csv') # Ruta al archivo CSV de KPIs (data/supply_chain_kpis.csv)
-kpi_df.to_csv(kpi_csv_path, index=False)                       # Guardo el DataFrame en un archivo CSV
+kpi_df.to_csv(kpi_csv_path, index=False, decimal=",")          # Guardo el DataFrame en un archivo CSV, se usa la "," como separador decimal
 print("Análisis completado. KPIs guardados en:", kpi_csv_path) # Imprimo la ruta del archivo generado como referencia
 
--- scripts/data_generation.py
+++ scripts/data_generation.py
@@ -2,12 +2,13 @@ import os
 from datetime import datetime, timedelta
 import random
 import pandas as pd
+import numpy as np
 
 script_dir = os.path.dirname(__file__)            # Carpeta base del script
 data_dir = os.path.join(script_dir, '..', 'data') # Ruta a la carpeta "data" (subiendo un nivel y luego /data)
 os.makedirs(data_dir, exist_ok=True)              # Me aseguro de que la carpeta existe
 
-num_registros = 5000 # Número de registros a generar
+num_registros = 10000 # Número de registros a generar
 
 # Función para generar fechas aleatorias entre 2 fechas dadas
 def random_date(start, end):
@@ -21,12 +22,14 @@ end_date = datetime(2023, 12, 31)
 # Genero los datos de forma aleatoria
 data = []
 for i in range(1, num_registros+1):
-    order_date = random_date(start_date, end_date)
-    lead_time = random.randint(5, 30)
+    
+    order_date = random_date(start_date, end_date)                      # Fecha de pedido
+    lead_time = int(np.clip(np.random.normal(8, 7), 3, 30))             #  Distribución normal para el lead time (media=8, std=7), acotado entre 3 y 30
     delivery_date = order_date + timedelta(days=lead_time)
-    on_time = random.choice(["Sí", "No"])
-    quality_score = round(random.uniform(7.0, 10.0), 1)
-    cost = round(random.uniform(1000, 10000), 2)
+    on_time = np.random.choice(["Sí", "No"], p=[0.8, 0.2])              # OnTimeDelivery con probabilidad 80% "Sí" y 20% "No"
+    quality_score = float(np.clip(np.random.normal(4, 1.2), 1, 5))      # Calidad con distribución normal (media=4, std=1.2), acotado entre 1 y 5
+    cost = float(np.clip(np.random.normal(5000, 2000), 500, 20000))     # Coste con distribución normal (media=5000, std=2000), acotado entre 500 y 20000
+    
     data.append({
         "OrderID": i,                                           # ID del pedido
         "SupplierID": f"S{random.randint(1,10)}",               # ID del proveedor
@@ -35,7 +38,7 @@ for i in range(1, num_registros+1):
         "DeliveryDate": delivery_date.strftime("%Y-%m-%d"),     # Fecha de entrega (YYYY-MM-DD)
         "OnTimeDelivery": on_time,                              # ¿Entrega a tiempo? (Sí/No)
         "LeadTime": lead_time,                                  # Tiempo de entrega (días)
-        "QualityScore": quality_score,                          # Calidad (7.0 - 10.0)
+        "QualityScore": quality_score,                          # Calidad (1 - 5)
         "Cost": cost                                            # Coste del pedido de compra ($)
     })
 
@@ -43,5 +46,5 @@ for i in range(1, num_registros+1):
 df = pd.DataFrame(data) # Creo un DataFrame con los datos generados
 
 csv_path = os.path.join(data_dir, 'supply_chain_data.csv')  # Ruta al archivo CSV de datos generados (data/supply_chain_data.csv)
-df.to_csv(csv_path, index=False)                            # Guardo el DataFrame en un archivo CSV
+df.to_csv(csv_path, index=False, float_format="%.2f", sep=';', decimal=',')       # Guardo el DataFrame en un archivo CSV
 print(f"Datos generados y guardados en {csv_path}")         # Imprimo la ruta del archivo generado como referencia
